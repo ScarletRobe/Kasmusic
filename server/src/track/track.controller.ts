df06062895e8c44f5bdbb47eef5629c62d6e765c
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-11 21:31:36 +0300

    Fix: update query Feat: comment query, picture + audio

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,15 +1,38 @@
 import { ObjectId } from 'mongoose';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { TrackService } from './track.service';
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { AddCommentDto } from './dto/add-comment.dto';
+import { FileFieldsInterceptor } from '@nestjs/platform-express/multer';
 
 @Controller('/tracks')
 export class TrackController {
   constructor(private trackService: TrackService) {}
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto);
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'picture', maxCount: 1 },
+      { name: 'audio', maxCount: 1 },
+    ]),
+  )
+  create(
+    @UploadedFiles()
+    files,
+    @Body() dto: CreateTrackDto,
+  ) {
+    const { picture, audio } = files;
+    return this.trackService.create(dto, picture[0], audio[0]);
   }
 
   @Get()
@@ -27,8 +50,13 @@ export class TrackController {
     return this.trackService.delete(id);
   }
 
-  @Post(':id')
+  @Patch(':id')
   update(@Param('id') id: ObjectId, @Body() dto: UpdateTrackDto) {
     return this.trackService.update(id, dto);
+  }
+
+  @Post('/comment')
+  addComment(@Body() dto: AddCommentDto) {
+    return this.trackService.addComment(dto);
   }
 }
--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,3 +1,4 @@
+import { FileService, FileType } from './../file/file.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
@@ -5,16 +6,25 @@ import mongoose, { Model, ObjectId } from 'mongoose';
 import { Comment, CommentDocument } from './schemas/comment.schema';
 import { Track, TrackDocument } from './schemas/track.schema';
 import { UpdateTrackDto } from './dto/update-track.dto';
+import { AddCommentDto } from './dto/add-comment.dto';
 
 @Injectable()
 export class TrackService {
   constructor(
     @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
     @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+    private fileService: FileService,
   ) {}
 
-  async create(dto: CreateTrackDto): Promise<Track> {
-    const track = await this.trackModel.create({ ...dto, listens: 0 });
+  async create(dto: CreateTrackDto, picture, audio): Promise<Track> {
+    const picturePath = this.fileService.createFile(FileType.IMAGE, picture);
+    const audioPath = this.fileService.createFile(FileType.AUDIO, audio);
+    const track = await this.trackModel.create({
+      ...dto,
+      listens: 0,
+      audio: audioPath,
+      picture: picturePath,
+    });
     return track;
   }
 
@@ -24,7 +34,7 @@ export class TrackService {
   }
 
   async getOne(id: ObjectId): Promise<Track> {
-    const track = await this.trackModel.findById(id);
+    const track = await this.trackModel.findById(id).populate('comments');
     return track;
   }
 
@@ -40,5 +50,13 @@ export class TrackService {
       { returnDocument: 'after' },
     );
     return track;
+  }
+
+  async addComment(dto: AddCommentDto): Promise<Comment> {
+    const track = await this.trackModel.findById(dto.trackId);
+    const comment = await this.commentModel.create({ ...dto });
+    track.comments.push(comment._id);
+    await track.save();
+    return comment;
   }
 }
