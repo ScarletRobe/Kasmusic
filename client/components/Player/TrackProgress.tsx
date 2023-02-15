e154d498d6e413eef616843b968df7d6d944c8b7
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-15 18:36:12 +0300

    Feat: Track Progress component

---
+++ client/components/Player/TrackProgress.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './Player.module.css';
+
+import { Box, Stack, Slider } from '@mui/material';
+
+interface TrackProgressProps {
+  left: number;
+  right: number;
+  onChange: (_, value: number | number[]) => void;
+}
+
+const TrackProgress: React.FC<TrackProgressProps> = ({
+  left,
+  right,
+  onChange,
+}) => {
+  return (
+    <Box ml="auto" mr={2} sx={{ flex: '1' }}>
+      <Stack direction="row" spacing={2} alignItems="center">
+        <Slider
+          aria-label="Progression"
+          min={0}
+          max={right}
+          value={left}
+          onChange={onChange}
+        />
+        <div className={styles.progressionValue}>
+          {left} / {right}
+        </div>
+      </Stack>
+    </Box>
+  );
+};
+
+export default TrackProgress;
