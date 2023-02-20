abbdb2ebffa37c99a298bfd167191b4afb0c62d7
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-20 17:45:34 +0300

    Feat: init empty tracks service

---
+++ client/services/tracksService.ts
@@ -0,0 +1,14 @@
+import { GetTracksParams, Track } from '../types/track';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { HYDRATE } from 'next-redux-wrapper';
+
+export const tracksApi = createApi({
+  reducerPath: 'tracks',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+  extractRehydrationInfo(action, { reducerPath }) {
+    if (action.type === HYDRATE) {
+      return action.payload[reducerPath];
+    }
+  },
+});
+
