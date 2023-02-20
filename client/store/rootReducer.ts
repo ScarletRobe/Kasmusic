f1da2051c45dc90116565d048d3b09b9b2d5287d
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-20 17:49:50 +0300

    Feat: connect tracks service to store

--- client/store/rootReducer.ts
+++ client/store/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { HYDRATE } from 'next-redux-wrapper';
 import { playerSlice } from './playerSlice/playerSlice';
+import { tracksApi } from '../services/tracksService';
 
 const rootReducer = combineReducers({
   player: playerSlice.reducer,
+  [tracksApi.reducerPath]: tracksApi.reducer,
 });
 
 export const reducer = (state, action) => {
--- client/store/store.ts
+++ client/store/store.ts
@@ -1,8 +1,18 @@
-import { reducer, RootState } from './rootReducer';
+import { tracksApi } from './../services/tracksService';
+import { reducer } from './rootReducer';
 import { configureStore, Store } from '@reduxjs/toolkit';
 import { Context, createWrapper } from 'next-redux-wrapper';
 
 const makeStore = (context: Context) =>
-  configureStore({ reducer: reducer, devTools: true });
+  configureStore({
+    reducer: reducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(tracksApi.middleware),
+  });
 
-export const wrapper = createWrapper<Store<RootState>>(makeStore);
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const wrapper = createWrapper<AppStore>(makeStore);
