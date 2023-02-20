69087afe2efed885a3be1e4302b323dee8b31b22
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-20 22:52:06 +0300

    Refactor: bring state into a component

--- client/components/Player/TrackProgress.tsx
+++ client/components/Player/TrackProgress.tsx
@@ -2,26 +2,27 @@ import React from 'react';
 import styles from './Player.module.css';
 
 import { Box, Stack, Slider } from '@mui/material';
+import { useTypedSelector } from '@/hooks/useTypedSelector';
+import { getFormattedTime } from '@/utils';
 
 interface TrackProgressProps {
-  left: number;
-  right: number;
   onChange: (_, value: number | number[]) => void;
 }
 
-const TrackProgress: React.FC<TrackProgressProps> = ({
-  left,
-  right,
-  onChange,
-}) => {
+const TrackProgress: React.FC<TrackProgressProps> = ({ onChange }) => {
+
+  // Player destructuring causes rerender when any state property is changed even unused ones
+  const duration = useTypedSelector((state) => state.player.duration);
+  const currentTime = useTypedSelector((state) => state.player.currentTime);
+
   return (
     <Box ml="auto" mr={2} sx={{ flex: '1' }}>
       <Stack direction="row" spacing={2} alignItems="center">
         <Slider
           aria-label="Progression"
           min={0}
-          max={right}
-          value={left}
+          max={duration}
+          value={currentTime}
           onChange={onChange}
         />
         <div className={styles.progressionValue}>
