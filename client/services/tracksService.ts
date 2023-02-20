8acd2453776723aeee1177912a36f1d9fcd16022
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-20 17:48:49 +0300

    Feat: getAllTracks endpoint

--- client/services/tracksService.ts
+++ client/services/tracksService.ts
@@ -10,5 +10,23 @@ export const tracksApi = createApi({
       return action.payload[reducerPath];
     }
   },
+  endpoints: (builder) => ({
+    getAllTracks: builder.query<Track[], GetTracksParams>({
+      query: ({ count = '25', offset = '0' }) => ({
+        url: '/tracks',
+        params: {
+          count,
+          offset,
+        },
+      }),
+    }),
 });
 
+export const {
+  useGetAllTracksQuery,
+  util: { getRunningQueriesThunk },
+} = tracksApi;
+
+export const {
+  getAllTracks,
+} = tracksApi.endpoints;
