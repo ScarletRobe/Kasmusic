1920daa724b2946318275f57a190c55ef7835a2e
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-20 17:52:47 +0300

    Feat: change mock with track list from server

--- client/components/TrackList.tsx
+++ client/components/TrackList.tsx
@@ -1,13 +1,19 @@
-import { Grid, Box } from '@mui/material';
 import React from 'react';
-import { Track } from '../types/track';
-import TrackItem from './TrackItem/TrackItem';
 
-interface TrackListProps {
+import TrackItem from './TrackItem/TrackItem';
+import { Track } from '@/types/track';
+
+import { Grid, Box } from '@mui/material';
+
+type TrackListProps = {
   tracks: Track[];
-}
+};
 
 const TrackList: React.FC<TrackListProps> = ({ tracks }) => {
+  if (!tracks) {
+    return <div>Ошибка</div>;
+  }
+
   return (
     <Grid container direction="column">
       <Box p={2}>
--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import { Grid, Card, Box, Button } from '@mui/material';
 import TrackList from '@/components/TrackList';
+import { wrapper } from '@/store/store';
+import { getAllTracks, getRunningQueriesThunk } from '@/services/tracksService';
+import { GetTracksParams, Track } from '@/types/track';
 
-const Index = () => {
+const Index: React.FC<{ tracks: Track[] }> = ({ tracks }) => {
   const router = useRouter();
 
   return (
@@ -17,11 +20,22 @@ const Index = () => {
             </Button>
           </Grid>
         </Box>
-        <TrackList
-        />
+        <TrackList tracks={tracks} />
       </Card>
     </Grid>
   );
 };
 
 export default Index;
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    store.dispatch(getAllTracks.initiate({} as GetTracksParams));
+    const tracks = (
+      await Promise.all(store.dispatch(getRunningQueriesThunk()))
+    )[0].data;
+    return {
+      props: { tracks },
+    };
+  },
+);
