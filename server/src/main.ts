c7671f1464c5de3ff6714ccf6f4fbd7a8dd7c50e
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-23 17:56:01 +0300

    Fix: cors 413 error

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,9 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    app.enableCors();
+    app.enableCors({
+      origin: true,
+    });
     await app.listen(PORT, () =>
       console.log(`server started on port: ${PORT}`),
     );
