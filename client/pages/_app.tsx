dae435cc75bd4548c2cd47e20dd87f5e284087e2
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-23 19:56:11 +0300

    Fix: set referer to no-referer

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,6 @@
 import MainLayout from '@/layouts/MainLayout';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { wrapper } from '../store/store';
 
@@ -9,11 +10,17 @@ function MyApp({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   const { pageProps } = props;
   return (
-    <Provider store={store}>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
-    </Provider>
+    <>
+      <Head>
+        <title>Тестик</title>
+        <meta name="referrer" content="no-referrer"></meta>
+      </Head>
+      <Provider store={store}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </Provider>
+    </>
   );
 }
 
