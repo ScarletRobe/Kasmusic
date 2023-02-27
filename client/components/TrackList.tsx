29b5e4bde4304e10f6dce7ab12fb5a294de9b5d4
Leonid Chernyakov <scrltrb@gmail.com>
2023-02-27 21:08:19 +0300

    Feat: implement search component

--- client/components/TrackList.tsx
+++ client/components/TrackList.tsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TrackItem from './TrackItem/TrackItem';
 
 import { Grid, Box } from '@mui/material';
+
 import { useGetAllTracksQuery } from '@/services/tracksService';
+import Searchbar from './Searchbar';
+import { Track } from '@/types/track';
 
 const TrackList: React.FC = () => {
-  const { data, isLoading, isError } = useGetAllTracksQuery({
+  const [prevReqId, setPrevReqId] = useState<null | string>(null);
+  const { data, isLoading, isError, status, requestId } = useGetAllTracksQuery({
     count: '50',
     offset: '0',
   });
+  const [tracks, setTracks] = useState<null | Track[]>(null);
+
+  if (status === 'fulfilled' && requestId && requestId !== prevReqId) {
+    if (data) {
+      setTracks(data);
+    }
+    setPrevReqId(requestId);
+  }
 
   if (isLoading) return <div>Loading</div>;
-  if (isError) return <div>Error</div>;
-  if (!data) return null;
+  if (isError || !tracks) return <div>Error</div>;
 
   return (
-    <Grid container direction="column">
-      <Box p={2}>
-        {data.map((track) => (
-          <TrackItem key={track._id} track={track} />
-        ))}
+    <>
+      <Box pl={4} pr={4}>
+        <Searchbar setTracks={setTracks} />
+        <Grid container direction="column">
+          <Box pt={2} pb={2}>
+            {tracks.map((track) => (
+              <TrackItem key={track._id} track={track} />
+            ))}
+          </Box>
+        </Grid>
       </Box>
-    </Grid>
+    </>
   );
 };
 
