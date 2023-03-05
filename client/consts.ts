d3420318fd96fc7b2fad9607b086d1c611ef12dd
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-05 22:20:46 +0300

    Feat: sort types

--- client/consts.ts
+++ client/consts.ts
@@ -1,3 +1,11 @@
+// export const BASE_SERVER_URL = 'http://localhost:5000/';
 export const BASE_SERVER_URL = 'https://music-platform-back.vercel.app/';
 export const TRACKS_URL = `${BASE_SERVER_URL}tracks/`;
 export const GET_MEDIA_BASE_URL = 'https://getfile.dokpub.com/yandex/get/';
+
+export enum SortTypes {
+  MOST_LISTENED = 'По убыванию прослушиваний',
+  LEAST_LISTENED = 'По возрастанию прослушиваний',
+  NEWEST = 'Последние добавленные',
+  LATEST = 'Первые добавленные',
+}
