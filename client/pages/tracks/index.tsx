ada41a48c45d67b9489081e935bb0b0bb28f53cc
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-06 21:57:46 +0300

    Feat: change sort back to default when page load

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Grid, Card, Box, Button } from '@mui/material';
 import TrackList from '@/components/TrackList';
@@ -6,9 +6,14 @@ import { Track } from '@/types/track';
 import Head from 'next/head';
 import { useTypedSelector } from '@/hooks/useTypedSelector';
 import { useGetAllTracksQuery } from '@/services/tracksService';
+import { SortTypes } from '@/consts';
+import { setCurrentSort } from '@/store/appSlice/appSlice';
+import { useDispatch } from 'react-redux';
 
 const Index: React.FC<{ tracks: Track[] }> = () => {
   const router = useRouter();
+  const dispatch = useDispatch();
+
   const currentSort = useTypedSelector((state) => state.app.currentSort);
 
   const { data, isLoading, isError } = useGetAllTracksQuery({
@@ -16,6 +21,10 @@ const Index: React.FC<{ tracks: Track[] }> = () => {
     offset: '0',
     sort: currentSort,
   });
+
+  useEffect(() => {
+    dispatch(setCurrentSort(SortTypes.NEWEST));
+  }, []);
 
   return (
     <>
