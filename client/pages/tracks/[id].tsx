1c93a2f2b8bc99b1a6327f2ce8e79c265ae99439
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-11 16:17:39 +0300

    Feat: ability to edit track info, styles

--- client/pages/tracks/[id].tsx
+++ client/pages/tracks/[id].tsx
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
-import { Track } from '../../types/track';
+import React from 'react';
 import { useRouter } from 'next/router';
-import { Button, Card, Grid, TextField } from '@mui/material';
-import styles from '../../styles/trackPage.module.css';
+
+import { wrapper } from '@/store/store';
+import { EditableFields, Track } from '../../types/track';
+import { useInput } from '@/hooks/useInput';
+import { GET_MEDIA_BASE_URL } from '@/consts';
 import {
   getRunningQueriesThunk,
   getTrackById,
   useCreateCommentMutation,
+  useEditTrackMutation,
   useGetTrackByIdQuery,
 } from '@/services/tracksService';
-import { wrapper } from '@/store/store';
-import { useInput } from '@/hooks/useInput';
-import { GET_MEDIA_BASE_URL } from '@/consts';
+
+import { Button, Card, Grid, TextField } from '@mui/material';
+
+import styles from '../../styles/trackPage.module.css';
+import EditableText from '@/components/EditableText';
+import { Stack } from '@mui/system';
+
+export type EditTrackInfoParams = {
+  field: EditableFields;
+  newValue: string;
+};
 
 const TrackPage: React.FC = () => {
   const router = useRouter();
@@ -19,10 +30,18 @@ const TrackPage: React.FC = () => {
 
   const { data, isLoading, isError } = useGetTrackByIdQuery(id as string);
   const [createComment] = useCreateCommentMutation();
+  const [editTrack] = useEditTrackMutation();
+
   const track = data;
 
   const username = useInput('');
   const text = useInput('');
+
+  const editTrackInfo = ({ field, newValue }: EditTrackInfoParams) => {
+    if (track) {
+      editTrack({ id: track._id, field, newValue });
+    }
+  };
 
   const addComment = () => {
     if (track) {
@@ -49,7 +68,7 @@ const TrackPage: React.FC = () => {
         <Button variant={'outlined'} onClick={() => router.push('/tracks')}>
           К списку
         </Button>
-        <Grid container className={styles.trackInfoContainer}>
+        <Grid container gap={3} className={styles.trackInfoContainer}>
           <img
             className={styles.trackImg}
             src={GET_MEDIA_BASE_URL + track.picture.url}
@@ -57,11 +76,24 @@ const TrackPage: React.FC = () => {
             height={200}
             alt="Track cover"
           />
-          <div className={styles.trackInfo}>
-            <h1>{track.name}</h1>
-            <h1>Исполнитель: {track.artist}</h1>
-            <h1>Прослушиваний: {track.listens}</h1>
-          </div>
+          <Stack gap={3} className={styles.trackInfo}>
+            <EditableText
+              text={track.name}
+              fieldName={EditableFields.NAME}
+              action={editTrackInfo}
+            />
+            <Stack direction="row" gap={1}>
+              <h1 className={styles.trackHeader}>Исполнитель:</h1>
+              <EditableText
+                text={`${track.artist}`}
+                fieldName={EditableFields.ARTIST}
+                action={editTrackInfo}
+              />
+            </Stack>
+            <h1 className={styles.trackHeader}>
+              Прослушиваний: {track.listens}
+            </h1>
+          </Stack>
         </Grid>
         <h2>Добавить комментарий</h2>
         <Grid container>
