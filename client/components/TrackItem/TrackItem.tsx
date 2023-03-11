3f0f21a650041c152f0237290ad324ebe3174620
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-11 22:40:23 +0300

    Refactor: connect set active track hook

--- client/components/TrackItem/TrackItem.tsx
+++ client/components/TrackItem/TrackItem.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Track } from '../../types/track';
-import styles from './TrackItem.module.css';
 import { useRouter } from 'next/router';
+import { useDispatch } from 'react-redux';
+
+import { GET_MEDIA_BASE_URL } from '@/consts';
+import { Track } from '../../types/track';
+
+import { useDeleteTrackMutation } from '@/services/tracksService';
+
+import { useTypedSelector } from '@/hooks/useTypedSelector';
+import { useSetActiveTrack } from '@/hooks/useSetActiveTrack';
+
 import { PlayArrow, Pause, Delete } from '@mui/icons-material';
 import { Card, IconButton, Grid } from '@mui/material';
 import RemoveRedEyeRoundedIcon from '@mui/icons-material/RemoveRedEyeRounded';
-import {
-  setActiveTrack,
-  setPause,
-  setPlay,
-} from '@/store/playerSlice/playerSlice';
-import { useDispatch } from 'react-redux';
-import { useTypedSelector } from '@/hooks/useTypedSelector';
-import {
-  useDeleteTrackMutation,
-  useIncrementListensMutation,
-} from '@/services/tracksService';
-import { GET_MEDIA_BASE_URL } from '@/consts';
+
+import styles from './TrackItem.module.css';
 
 interface TrackItemProps {
   track: Track;
@@ -24,27 +22,13 @@ interface TrackItemProps {
 
 const TrackItem: React.FC<TrackItemProps> = ({ track }) => {
   const router = useRouter();
-  const dispatch = useDispatch();
-  const [deleteTrack, { isLoading, isError }] = useDeleteTrackMutation();
-  const [incListens] = useIncrementListensMutation();
+  const [deleteTrack] = useDeleteTrackMutation();
 
   // Player destructuring causes rerender when any state property is changed even unused ones
   const activeTrack = useTypedSelector((state) => state.player.activeTrack);
   const pause = useTypedSelector((state) => state.player.pause);
 
-  const currentSort = useTypedSelector((state) => state.app.currentSort);
-
-  const play = (e: React.MouseEvent) => {
-    e.stopPropagation();
-
-    if (activeTrack?._id === track._id) {
-      pause ? dispatch(setPlay()) : dispatch(setPause());
-    } else {
-      dispatch(setPause());
-      dispatch(setActiveTrack({ ...track }));
-      incListens({ id: track._id, sort: currentSort });
-    }
-  };
+  const { play } = useSetActiveTrack(track);
 
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
