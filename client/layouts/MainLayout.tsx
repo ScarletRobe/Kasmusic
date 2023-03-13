139310bbe0ecb6021c88cecf343d478760050349
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-13 15:52:02 +0300

    Feat: favicon

--- client/layouts/MainLayout.tsx
+++ client/layouts/MainLayout.tsx
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container';
 import Player from '@/components/Player/Player';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '@/styles/MUITheme';
+import Head from 'next/head';
 
 interface MainLayoutProps {
   children: JSX.Element;
@@ -12,6 +13,27 @@ interface MainLayoutProps {
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Kasmusic</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="../static/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../static/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../static/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="../static/favicon/site.webmanifest" />
+      </Head>
       <ThemeProvider theme={theme}>
         <Navbar />
         <Container style={{ marginTop: '90px', paddingBottom: '65px' }}>
