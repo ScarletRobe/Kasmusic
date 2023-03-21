4f9e35f18f6e6f628712b2066732002981a65f67
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-21 19:57:58 +0300

    Feat: user schema

---
+++ server/src/user/schemas/user.schema.ts
@@ -0,0 +1,32 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { Roles } from './../enums';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ unique: true, required: true })
+  email: string;
+
+  @Prop({ default: false })
+  isActivated: boolean;
+
+  @Prop()
+  activationLink: string;
+
+  @Prop({ unique: true, required: true })
+  username: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  avatarLink: string;
+
+  @Prop({ type: [{ type: String, ref: 'Role' }], default: [Roles.USER] })
+  roles;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
