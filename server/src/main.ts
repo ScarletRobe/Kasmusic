05f281b2e02daab10e5810d53485ea004c6fd4aa
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-23 02:02:53 +0300

    Feat: connect cookie parser

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
-import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from './app.module';
 
 const start = async () => {
   try {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
     app.enableCors({
       origin: true,
     });
