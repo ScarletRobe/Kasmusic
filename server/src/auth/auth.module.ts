1b9810e5ab6a9fafab3da80a56c2899de2bcca5a
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-22 20:43:02 +0300

    Fix: incorrect import path

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '../user/user.module';
 
 import { AuthController } from './auth.controller';
 
