9cb3f9533bac4a111dff79f0d25b4b066759ec54
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-23 16:17:14 +0300

    Feat: roles guard

---
+++ server/src/common/guards/roles.guard.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Roles } from './../../user/enums';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const [req] = context.getArgs();
+    const roles = req?.user?.roles || [];
+    const requiredRoles = this.reflector.getAllAndOverride<Roles[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    const hasAllRequiredRoles = requiredRoles.every((reqRole) =>
+      roles.includes(reqRole),
+    );
+
+    if (!requiredRoles.length || hasAllRequiredRoles) {
+      return true;
+    }
+
+    throw new ForbiddenException("You don't have necessary permissions");
+  }
+}
