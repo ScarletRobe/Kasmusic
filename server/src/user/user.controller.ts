2a43235cf4c6427e509c5ea658c5762a1a0f45c3
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-23 20:51:36 +0300

    Feat: guard private routes for user controlller

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,20 +1,29 @@
+import { RolesGuard } from './../common/guards/roles.guard';
 import { Body, Controller, Post } from '@nestjs/common';
-import { Get, Param } from '@nestjs/common/decorators';
+import { Get, Param, UseGuards } from '@nestjs/common/decorators';
+import { RequiredRoles } from '../common/decorators/requiredRoles.decorator';
 
 import { UserService } from './user.service';
 
 import { CreateRoleDto } from './dto/create-role.dto';
+import { AccessTokenGuard } from '../common/guards/accessToken.guard';
+
+import { Roles } from './enums';
 
 @Controller('/user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('/update/:id')
+  @RequiredRoles(Roles.ADMIN)
+  @UseGuards(AccessTokenGuard, RolesGuard)
   async update(@Body() params, @Param() { id }) {
     return await this.userService.findByIdAndUpdate(id, params);
   }
 
   @Post('/role')
+  @RequiredRoles(Roles.ADMIN)
+  @UseGuards(AccessTokenGuard, RolesGuard)
   async createRole(@Body() dto: CreateRoleDto) {
     try {
       await this.userService.createRole(dto);
@@ -27,6 +36,8 @@ export class UserController {
   }
 
   @Get(':id')
+  @RequiredRoles(Roles.ADMIN)
+  @UseGuards(AccessTokenGuard, RolesGuard)
   findOne(@Param('id') id: string) {
     try {
       return this.userService.findOne({ _id: id });
