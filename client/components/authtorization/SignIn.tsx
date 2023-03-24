9546f0990bb823bf88481a25e8af8751ac3aaf30
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-24 21:07:19 +0300

    Feat: signIn component markup

---
+++ client/components/authtorization/SignIn.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+import PasswordField from './textFields/PasswordField';
+import UsernameField from './textFields/UsernameField';
+
+import { Box, FormControl, Button } from '@mui/material';
+
+const INITIAL = { text: '', error: '' };
+
+const SignIn = () => {
+  const [username, setUsername] = React.useState(INITIAL);
+  const [password, setPassword] = React.useState(INITIAL);
+  const [loading, setLoading] = React.useState(false);
+
+  return (
+    <Box p={2}>
+      <UsernameField {...{ username, setUsername, loading }} />
+      <PasswordField {...{ password, setPassword, loading }} />
+
+      <FormControl margin="normal" fullWidth>
+        <Button
+          style={{ textTransform: 'none' }}
+          size="large"
+          disabled={loading}
+          variant="contained"
+          color="primary"
+          fullWidth
+          // onClick={handleSubmit}
+        >
+          Войти
+        </Button>
+      </FormControl>
+    </Box>
+  );
+};
+export default SignIn;
