62263a1ef1b7bea35eaec7baded9c6541e386fcd
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-26 19:04:54 +0300

    Feat: validation for signup form

--- client/components/authorization/SignUp.tsx
+++ client/components/authorization/SignUp.tsx
@@ -1,19 +1,62 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+
+import { validate } from '@/helpers/validate';
+import { useSignUpMutation } from '@/services/authService';
 
 import EmailField from './textFields/EmailField';
 import PasswordField from './textFields/PasswordField';
 import UsernameField from './textFields/UsernameField';
 
 import { Box, FormControl, Button } from '@mui/material';
-// import checkValid from '../../util/checkvalid';
 
 const INITIAL = { text: '', error: '' };
 
 const SignUp: React.FC = () => {
+  const [signUp] = useSignUpMutation();
+
+  const [username, setUsername] = useState(INITIAL);
+  const [email, setEmail] = useState(INITIAL);
+  const [password, setPassword] = useState(INITIAL);
+  const [isError, setIsError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async () => {
+    setIsError(false);
+    if (
+      [
+        validate(
+          email.text,
+          { notEmpty: true, emailPattern: true, maxLength: 30 },
+          {
+            setErrorMessage: (message: string) =>
+              setEmail((state) => ({ ...state, error: message })),
+          },
+        ),
+        validate(
+          username.text,
+          { notEmpty: true, maxLength: 30, minLength: 3 },
+          {
+            setErrorMessage: (message: string) =>
+              setUsername((state) => ({ ...state, error: message })),
+          },
+        ),
+        validate(
+          password.text,
+          { notEmpty: true, maxLength: 30, minLength: 3 },
+          {
+            setErrorMessage: (message: string) =>
+              setPassword((state) => ({ ...state, error: message })),
+          },
+        ),
+      ].some((v) => !v)
+    ) {
+      return;
+    }
   const [username, setUsername] = React.useState(INITIAL);
   const [email, setEmail] = React.useState(INITIAL);
   const [loading, setLoading] = React.useState(false);
   const [password, setPassword] = React.useState(INITIAL);
+  };
 
   return (
     <Box p={2}>
@@ -29,7 +72,7 @@ const SignUp: React.FC = () => {
           variant="contained"
           color="primary"
           fullWidth
-          // onClick={handleSubmit}
+          onClick={handleSubmit}
         >
           Зарегистрировать
         </Button>
