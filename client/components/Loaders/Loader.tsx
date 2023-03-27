d22ea20dbb06b7e1f76137605cdcdcd90091b448
Leonid Chernyakov <scrltrb@gmail.com>
2023-03-27 23:08:55 +0300

    Feat: small version for loader

--- client/components/Loaders/Loader.tsx
+++ client/components/Loaders/Loader.tsx
@@ -2,16 +2,38 @@ import React from 'react';
 
 import styles from './loader.module.css';
 
-const Loader = () => {
+enum LoaderSize {
+  Small = 'Small',
+  Default = 'Default',
+}
+
+type LoaderProps = {
+  color?: string;
+  size?: keyof typeof LoaderSize;
+};
+
+const Loader = ({
+  size = LoaderSize.Default,
+  color = '#673ab7',
+}: LoaderProps) => {
+  let className;
+  switch (size) {
+    case LoaderSize.Small:
+      className = styles.loaderSmall;
+      break;
+    default:
+      break;
+  }
+
   return (
-    <div className={styles.loader}>
-      <span className={styles.stroke}></span>
-      <span className={styles.stroke}></span>
-      <span className={styles.stroke}></span>
-      <span className={styles.stroke}></span>
-      <span className={styles.stroke}></span>
-      <span className={styles.stroke}></span>
-      <span className={styles.stroke}></span>
+    <div className={`${styles.loader} ${className}`}>
+      <span style={{ background: color }} className={styles.stroke}></span>
+      <span style={{ background: color }} className={styles.stroke}></span>
+      <span style={{ background: color }} className={styles.stroke}></span>
+      <span style={{ background: color }} className={styles.stroke}></span>
+      <span style={{ background: color }} className={styles.stroke}></span>
+      <span style={{ background: color }} className={styles.stroke}></span>
+      <span style={{ background: color }} className={styles.stroke}></span>
     </div>
   );
 };
